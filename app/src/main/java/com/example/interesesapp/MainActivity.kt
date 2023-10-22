b3cf982fb52d884b93092d0c5005bb4d38861c3e
yessiosorio <osorioyessi109@gmail.com>
2023-10-21 19:12:47 -0600

    se agrego

---
+++ app/src/main/java/com/example/interesesapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.interesesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.interesesapp.ui.theme.InteresesAppTheme
+import com.example.interesesapp.ui.theme.viewmodels.PrestamoViewModel
+import com.example.interesesapp.ui.theme.views.HomeView
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewModel: PrestamoViewModel by viewModels()
+        setContent {
+            InteresesAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeView(viewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InteresesAppTheme {
+        Greeting("Android")
+    }
+}
